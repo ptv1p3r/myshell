23a7100e947e283c7b3e67deb64b12819a87870b
Pedro Roldan <pedro.roldan@gmail.com>
2020-10-21 10:47:59 +0100

    .

--- myshell.sh
+++ myshell.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-echo "teste ok"
+printf "myshell> "
+
+# get commands
+read command options
+
+# display freespace of device
+getFreespace(){
+	df -h /dev/sda1
+}
+
+#display pid of process in ascending order
+getPid(){
+	printf "pid " $1
+}
+
+case $command in
+	pid)	getPid options	;;	
+	freespace) getFreespace ;;
+	exit) printf "Pedro Roldan shell exit\n" ; exit ;
+esac
+
+
